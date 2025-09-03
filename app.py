ce0faf0ea106273cad61c0c20dd11c98ff8317a8
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-09-03 17:47:20 -0300

    escrita do codigo

---
+++ app.py
@@ -0,0 +1,5 @@
+nome="Emily"
+idade = 15
+linguagem_de_programação_favorita = 'python'
+mensagem = f"Oi meu nome é {nome}, tenho {idade} anos de idade e minha linguagem de programação favorita é {linguagem_de_programação_favorita}"
+print(mensagem)
