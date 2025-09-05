1b688aca7ba65d4e9e7ef6f60614801bd4d315d1
Emily <emily.ferreira-youxlab@institutoyoux.org.br>
2025-09-05 14:29:25 -0300

    alterei o código

--- src/app.py
+++ src/app.py
@@ -3,4 +3,4 @@ idade = int(input('Sua idade é? ;'))
 linguagem = input('Linguagem favorita?: ')
 ano_atual = 2025
 ano_nascimento = ano_atual - idade
-print(f'Meu nome: {nome}\n Minha idade: {idade}\n Linguagem favorita: {linguagem}\n Ano de nascimento: {ano_nascimento}')
+print(f'Meu nome: {nome}\n Minha idade: {idade}\n Linguagem  favorita: {linguagem}\n Ano de nascimento: {ano_nascimento}')
